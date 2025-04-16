53d73b905135632775248e6ee5aff48bec7b2624
Saurabhbokde01 <141599785+Saurabhbokde01@users.noreply.github.com>
2025-04-16 11:53:50 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb+srv://bokadesaurabh88:Saurabh2004@cluster0.64ik2.mongodb.net/chemical').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://bokadesaurabh88:Saurabh2004@cluster0.64ik2.mongodb.net/food-del').then(()=>console.log("DB Connected"));
    
 }
 
